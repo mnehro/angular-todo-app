499f4d2cadaf2c11c3273dd2115a557ec86fca1f
mnehro <mnehro@madfoat.com>
2024-06-30 14:09:02 +0300

    http service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { EventService } from '../shared/services/EventService';
+import { WishService } from './wish.service';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,19 +16,18 @@ import { EventService } from '../shared/services/EventService';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  items: WishItem[] = [
-    new WishItem('To learn angular'),
-    new WishItem('Get coffe', true),
-    new WishItem('Find grass that cut itself')
-  ];
+  items: WishItem[] = [];
   filter: any;
-  constructor(events: EventService) {
+  constructor(events: EventService, private wishService : WishService) {
     events.listen('removeWish', (wish: WishItem) => {
       const index = this.items.indexOf(wish);
       this.items.splice(index, 1);
     });
   }
   ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data : any) => {
+      this.items = data;
+    })
     this.filter = (item: WishItem) => item;
   }
 
---
+++ src/app/wish.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WishItem } from '../shared/models/wishItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WishService {
+
+  constructor(private http: HttpClient) { }
+
+  getWishes() : Observable<WishItem[]> {
+    return this.http.get<WishItem[]>('wishes.json');
+  }
+}
