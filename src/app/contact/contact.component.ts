6c9264e19caba929cf4ce79d2716522dd0fc8bb6
mnehro <mnehro@madfoat.com>
2024-07-01 14:43:22 +0300

    custom validator

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { createInvalidDomainValidator } from './invalidEmailDomain';
+
+const invalidEmailDomain = createInvalidDomainValidator(['gmail.com', 'yahoo.com']);
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,7 +12,7 @@ export class ContactComponent {
 
   contactForm = new FormGroup({
     senderName: new FormControl('', Validators.required),
-    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderEmail: new FormControl('', [Validators.required, Validators.email, invalidEmailDomain]),
     senderMessage: new FormControl('', [Validators.required, Validators.minLength(10)])
   });
 
